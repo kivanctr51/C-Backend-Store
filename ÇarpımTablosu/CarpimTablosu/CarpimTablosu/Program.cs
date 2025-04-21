e3a52d818fe186db0375d324a03eac3cd5624e98
Fikret Gıvanç <98213779+kivanctr51@users.noreply.github.com>
2025-04-21 23:56:28 +0300

    C# ile ÇarpıM Tablosu

---
+++ ÇarpımTablosu/CarpimTablosu/CarpimTablosu/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("çarpım tablosuna hoşgeldiniz");
+Thread.Sleep(2000);
+for (int i = 1; i <= 10; i++)
+{
+    for (int j = 1; j <= 10; j++)
+    {
+        Console.Write($"  {i} x  {j}  =  {i * j }" );
+    }
+    Console.WriteLine();
+}

4dd8f2e25448791abf32e0da7f5a97062657337e
Fikret Gıvanç <98213779+kivanctr51@users.noreply.github.com>
2025-04-22 00:00:47 +0300

    Add files via upload

---
+++ DonguOrtalama/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.WriteLine("Hoşgeldiniz: toplam 5 sayi girilicek");
+Thread.Sleep(3000);
+Console.Clear();
+
+
+Console.Write("1. Sayıyı giriniz: ");
+int Sayi1 =int.Parse(Console.ReadLine());
+Console.Write("2. Sayıyı giriniz: ");
+int Sayi2 =int.Parse(Console.ReadLine());
+Console.Write("3. Sayıyı giriniz: ");
+int Sayi3 =int.Parse(Console.ReadLine());
+Console.Write("4. Sayıyı giriniz: ");
+int Sayi4 =int.Parse(Console.ReadLine());
+Console.Write("5. Sayıyı giriniz: ");
+int Sayi5 =int.Parse(Console.ReadLine());
+
+List<int> Data = new List<int>{Sayi1,Sayi2,Sayi3,Sayi4,Sayi5};
+
+int toplam = 0;
+
+for (int i = 0; i < Data.Count; i++)
+{
+    toplam += Data[i];
+}
+
+double ortalama = (double)toplam / Data.Count;
+
+Console.WriteLine("ortalamanız : " + ortalama);
---
+++ Döngü ile hesap makinesi/DonguHesapMakinesi/DonguHesapMakinesi/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("merhaba kaç tane sayı toplamamak istiyorsun");
+int UserLogin = int.Parse(Console.ReadLine());
+
+
+int total = 0;
+int count = 0;
+
+
+for (int i = 1; i <= UserLogin; i++)
+{
+    Console.Write($"{i}. sayıyı girin: ");
+    count = int.Parse(Console.ReadLine());
+    total += count;
+    
+}
+
+Console.WriteLine($"toplama işlemi sonucu :  {total}");
